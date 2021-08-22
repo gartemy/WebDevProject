fc8b272773dc912c123402a3c523b9d934a3d1b6
gartemy <artemganzhaa@gmail.com>
2021-08-22 23:58:28 +0500

    Mobile Menu commit

--- index.js
+++ index.js
@@ -1,38 +1,69 @@
-$(document).ready(() => {
-  $(".header__menu").click(function () {
-    $(".header").toggleClass("active");
-    $(this).toggleClass("active");
-    $(".header__navigation").slideToggle(300, function () {
-      if ($(this).css("display") === "none") {
-        $(this).removeAttr("style");
-      }
-    });
-  });
+// Declaring variables for working with the Mobile Menu
+const body = document.querySelector('body');
+const header = document.querySelectorAll('.header')[0];
+const headerMenu = document.querySelectorAll('.header__menu')[0];
+const headerNavigation = document.querySelectorAll('.header__navigation')[0];
+const headerLinks = document.querySelectorAll('.header__navigation-link');
 
-  $(
-    ".web-project__title, .web-project__text, .web-project__buttons, .web-project__arrow"
-  )
-    .hide()
-    .slideToggle(1500);
+// Mobile Menu (show when the menu icon is clicked)
+headerMenu.addEventListener('click', () => {
+  headerMenu.classList.toggle('active');
+  header.classList.toggle('active');
+  headerNavigation.classList.toggle('active');
+  body.classList.toggle('lock');
 });
 
+// Mobile Menu animation (hide when clicking a link)
+headerLinks.forEach((item) => {
+  item.addEventListener('click', () => {
+    if (window.innerWidth <= 880) {
+      headerMenu.classList.toggle('active');
+      header.classList.toggle('active');
+      headerNavigation.classList.toggle('active');
+      body.classList.toggle('lock');
+    }
+  });
+});
+
+// Show Mobile Menu when the screen is rotated (on mobile devices)
+window.addEventListener(
+  'orientationchange',
+  () => {
+    if (headerNavigation.style.display === 'none') {
+      headerNavigation.removeAttribute('style');
+    }
+  },
+  false
+);
+
+// Arrow-up animation
+window.onscroll = () => {
+  if (this.scrollY > 834) {
+    document.querySelectorAll('.arrow-up')[0].style.display = 'flex';
+  } else {
+    document.querySelectorAll('.arrow-up')[0].style.display = 'none';
+  }
+};
+
+// Smooth scrolling of anchors
 const anchors = document.querySelectorAll('a[href*="#"]');
 
 for (let anchor of anchors) {
-  anchor.addEventListener("click", function (e) {
+  anchor.addEventListener('click', function (e) {
     e.preventDefault();
 
-    const blockID = anchor.getAttribute("href").substr(1);
+    const blockID = anchor.getAttribute('href').substr(1);
 
-    if (blockID !== "") {
+    if (blockID !== '') {
       document.getElementById(blockID).scrollIntoView({
-        behavior: "smooth",
-        block: "start",
+        behavior: 'smooth',
+        block: 'start',
       });
     }
   });
 }
 
+// Carousel for Our-team section
 let slideIndex = 1;
 
 const currentSlide = (n) => {
@@ -40,17 +71,17 @@ const currentSlide = (n) => {
 };
 
 const showSlides = (n) => {
-  const slides = document.getElementsByClassName("our-team__items");
-  const dots = document.getElementsByClassName("our-team__slider-slide");
+  const slides = document.getElementsByClassName('our-team__items');
+  const dots = document.getElementsByClassName('our-team__slider-slide');
 
   for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = 'none';
   }
 
   for (let i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(' active', '');
   }
 
-  slides[slideIndex - 1].style.display = "flex";
-  dots[slideIndex - 1].className += " active";
+  slides[slideIndex - 1].style.display = 'flex';
+  dots[slideIndex - 1].className += ' active';
 };
